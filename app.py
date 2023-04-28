6fad43a3bf9511fffc87e97dce56c4f71b654c53
Karan Ramrakhani <kramrakh@usc.edu>
2023-04-28 03:18:24 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import re
+from urllib import response
 import openai
 from flask import Flask, request, render_template
 from io import BytesIO
@@ -11,9 +12,65 @@ app = Flask(__name__)
 
 summary_pipeline = pipeline('summarization')
 # risk_pipeline = pipeline('text-classification', model='distilbert-base-uncased', return_all_scores=True)
+openai.api_key = "API KEY"
 
-openai.api_key = "API KEY HERE"
 
+# def extract_due_diligence_questions(text):
+#     prompt = "Generate a list of key questions to ask during due diligence based on the following text:\n\n"
+#     chunk_size = 3900  # maximum context length minus some buffer
+#     chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+
+#     questions = []
+#     for chunk in chunks:
+#         response = openai.Completion.create(
+#             engine="text-davinci-003",
+#             prompt=prompt + chunk,
+#             max_tokens=100,
+#             n=1,
+#             stop=None,
+#             temperature=0.5
+#         )
+
+#         for line in response.choices[0].text.strip().split('\n'):
+#             question = line.strip()
+#             if question:
+#                 questions.append(question)
+
+#     return questions
+
+
+# def extract_financial_data(text):
+#     prompt = "Extract key financial data points such as revenue, EBITDA, and market share from the following text:\n\n"
+#     chunk_size = 3900  # maximum context length minus some buffer
+#     chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+
+#     financial_data = {}
+#     for chunk in chunks:
+#         response = openai.Completion.create(
+#             engine="text-davinci-003",
+#             prompt=prompt + chunk,
+#             max_tokens=100,
+#             n=1,
+#             stop=None,
+#             temperature=0.5
+#         )
+#         print("$$$$$$$$$$$$$$$")
+#         print(response.choices[0].text)
+#         # for key, value in response.choices[0].text.strip().split('\n'):
+#         #     key = key.strip()
+#         #     value = value.strip()
+#         #     if key and value:
+#         #         financial_data[key] = value
+
+#         for line in response.choices[0].text.strip().split('\n'):
+#             parts = line.strip().split(':')
+#             if len(parts) == 2:
+#                 key, value = parts
+#                 print(key + "  " + value)
+#                 financial_data[key.strip()] = value.strip()
+
+#     return financial_data
+# # ... rest of the app.py code
 
 def extract_financial_data(text):
     prompt = "Extract key financial data points such as revenue, EBITDA, and market share from the following text:\n\n"
@@ -30,23 +87,13 @@ def extract_financial_data(text):
             stop=None,
             temperature=0.5
         )
-        print("$$$$$$$$$$$$$$$")
-        print(response.choices[0].text)
-        # for key, value in response.choices[0].text.strip().split('\n'):
-        #     key = key.strip()
-        #     value = value.strip()
-        #     if key and value:
-        #         financial_data[key] = value
-
         for line in response.choices[0].text.strip().split('\n'):
             parts = line.strip().split(':')
             if len(parts) == 2:
                 key, value = parts
-                print(key + "  " + value)
                 financial_data[key.strip()] = value.strip()
 
     return financial_data
-# ... rest of the app.py code
 
 
 @app.route('/')
@@ -56,6 +103,14 @@ def index():
 # ... other parts of the code
 
 
+# def process_text_in_chunks(text, pipeline, chunk_size=450):
+#     tokens = text.split()
+#     chunks = [' '.join(tokens[i:i+chunk_size])
+#               for i in range(0, len(tokens), chunk_size)]
+#     results = []
+#     for chunk in chunks:
+#         results.extend(pipeline(chunk))
+#     return results
 def process_text_in_chunks(text, pipeline, chunk_size=450):
     tokens = text.split()
     chunks = [' '.join(tokens[i:i+chunk_size])
@@ -66,7 +121,42 @@ def process_text_in_chunks(text, pipeline, chunk_size=450):
     return results
 
 
+def extract_due_diligence(text):
+    prompt = "Generate a list of key questions to ask bankers and management teams based on the following text:\n\n"
+    chunk_size = 3900  # maximum context length minus some buffer
+    chunks = [text[i:i + chunk_size] for i in range(0, len(text), chunk_size)]
+
+    due_diligence = []
+    for chunk in chunks:
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt + chunk,
+            max_tokens=100,
+            n=1,
+            stop=None,
+            temperature=0.5
+        )
+        for line in response.choices[0].text.strip().split('\n'):
+            line = line.strip()
+            if line:
+                due_diligence.append(line)
+
+    return due_diligence
+
+
 # ... other parts of the code
+# def extract_sections(text):
+#     sections = []
+#     current_section = ""
+#     for line in text.split("\n"):
+#         if line.startswith("SECTION"):
+#             if current_section:
+#                 sections.append(current_section.strip())
+#                 current_section = ""
+#         current_section += line + "\n"
+#     sections.append(current_section.strip())
+#     return sections
+
 def extract_sections(text):
     sections = []
     current_section = ""
@@ -117,12 +207,13 @@ def upload():
         if final_summary_results:
             final_summary = ' '.join([result['summary_text']
                                      for result in final_summary_results])
+            # final_summary = cleanSummary(final_summary)
         else:
             final_summary = "No final summary available."
 
         financial_data = extract_financial_data(text)
-
-        return render_template('result.html', summary=final_summary, financial_data=financial_data)
+        due_diligence = extract_due_diligence(text)
+        return render_template('result.html', summary=final_summary, financial_data=financial_data, due_diligence=due_diligence)
 
     return 'Unsupported file type', 400
 
